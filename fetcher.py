c926ba40d7f6f259fbf7f96014b49b3e1b61eb7c
Matthew Koontz <mjk3979@rit.edu>
2014-05-30 02:51:14 -0700

    Wrapped make_even in try/catch

--- fetcher.py
+++ fetcher.py
@@ -12,25 +12,27 @@ regex_loc = re.compile('maps\\.google\\.com/maps\\?q=(.*?)\\&amp;', flags=re.I)
 
 
 def make_event_from_page(link):
-    f = urlopen(link)
-    soup = BeautifulSoup(f)
     event = Event()
-    main = soup.select(".sectionMain")[0]
     event.link = link
+    try:
+        f = urlopen(link)
+        soup = BeautifulSoup(f)
+        main = soup.select(".sectionMain")[0]
 
-    event.name = main.select(".subHeading")[0].get_text().strip()
+        event.name = main.select(".subHeading")[0].get_text().strip()
 
-    info = main.select(".secondaryContent")[0].get_text()
+        info = main.select(".secondaryContent")[0].get_text()
 
-    sstart = regex_start.search(info).group(1).strip()
-    event.start = datetime.strptime(sstart, date_format)
+        sstart = regex_start.search(info).group(1).strip()
+        event.start = datetime.strptime(sstart, date_format)
 
-    send = regex_end.search(info).group(1).strip()
-    event.end = datetime.strptime(send, date_format)
+        send = regex_end.search(info).group(1).strip()
+        event.end = datetime.strptime(send, date_format)
 
-    event.location = urllib.parse.unquote_plus(regex_loc.search(str(main)).group(1).strip())
-    
+        event.location = urllib.parse.unquote_plus(regex_loc.search(str(main)).group(1).strip())
 
+    except:
+        pass
     return event
 
 print(make_event_from_page('http://smashboards.com/threads/jun-15-2014-super-smash-sundays-east-coast-5-red-bank-new-jersey.356635/'))
