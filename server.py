77c742845ce3e27e906e17ac79d239fa8cd0b061
Matthew Koontz <mjk3979@rit.edu>
2014-06-15 19:24:10 -0700

    Made the server refresh its cache

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 from wsgiref.simple_server import make_server
 from tg import expose, TGController, AppConfig
 import pickle
-from datetime import datetime
+from datetime import datetime, timedelta
 
 EVENTS_CACHE = None
+LAST_UPDATED = None
+UPDATE_INTERVAL = timedelta(minutes=30)
 def fetchEvents():
-    global EVENTS_CACHE
+    global EVENTS_CACHE, LAST_UPDATED, UPDATE_INTERVAL
 
     def eventFilter(event):
         return event.start is not None and event.start >= datetime.today() \
             and event.location is not None \
             and event.coords is not None
 
-    if EVENTS_CACHE is None:
+    if EVENTS_CACHE is None or (LAST_UPDATED - datetime.now()) >= UPDATE_INTERVAL:
         with open('eventobjs.dat', 'rb') as f:
             events = pickle.load(f)
             events = [event.__dict__ for event in events if eventFilter(event)]
             EVENTS_CACHE = events
+            LAST_UPDATED = datetime.now()
 
     return EVENTS_CACHE
 
